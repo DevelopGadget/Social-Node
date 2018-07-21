8555269bcdb8bf708c9af50fa112e725fbb0c4bb
FernanPro18 <fernandoaraujo081@gmail.com>
2018-07-21 11:35:23 -0500

    Index archivos

---
+++ Routes/Router.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var User = require('../Controllers/UsuarioController');
+
+module.exports = function (app){
+  app.get('/User', User.Get);
+  app.post('/User', User.Post);
+  app.put('/User/:Id', User.Put);
+  app.delete('/User/:Id', User.Delete);
+  app.get('/User/:Id', User.GetId);
+}
