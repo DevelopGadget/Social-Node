6052e11092a29dbeeede8038404fb4c1561395cb
FernanPro18 <fernandoaraujo081@gmail.com>
2018-07-22 10:46:20 -0500

    mongoo

--- Index.js
+++ Index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const body = require('body-parser');
-
+const mongoose = require('mongoose');
 //Puerto
 app.set('port', process.env.PORT || 3000);
 
@@ -15,7 +15,11 @@ app.use(body.urlencoded({ extended: false }));
 
 //require('./Routes/Router')(app);
 
-//Listen
-app.listen(app.get('port'), () => {
-  console.log('Iniciado ' + app.get('port'));
+mongoose.connect('mongodb://root:Fernando16@ds147451.mlab.com:47451/social-node', (err, res) => {
+  if (err)  onsole.log('no conexion');
+  //Listen
+  app.listen(app.get('port'), () => {
+    console.log('Iniciado ' + app.get('port'));
+  });
 });
+
